74f3147ddb43b93d81743be932a51c3061102f8e
Seonjoo Lee <seonjoolee@seonjoosMacBook.home>
2018-04-17 20:15:02 -0400

    initial

---
+++ R/propen_weight.R
@@ -0,0 +1,13 @@
+#' Conduct Propensity Weithing for Neuroimaging Data
+#'
+#' ADD DESCRIPTION
+#'
+
+#' @param filelist Full pathList of imaiging files.
+#' @return z-map
+#' @examples
+#' ## CREATE EXAMPLE PLEASE.
+hello <- function(filelist) {
+  print("Hello, world!")
+  return(zmap)
+}
